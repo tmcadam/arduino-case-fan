e304c154ca6dc0ece3622e77e9c129f6d7a39be0
Tom McAdam <mcadam1980@gmail.com>
2026-01-20 12:23:26 +0900

    Add a system idle check to allow sleep

--- python/main.py
+++ python/main.py
@@ -1,7 +1,15 @@
+from datetime import datetime
+from fileinput import filename
+import os
 import serial
 import serial.tools.list_ports
 import time
 import winreg
+import psutil
+
+from datetime import datetime
+
+IDL_FNAME = "C:\\Data\\idle.log"
 
 MAGIC = "MAGIC_CT_SENSOR"
 BAUD = 9600
@@ -9,6 +17,28 @@ SCAN_TIMEOUT = 2
 OUTPUT_FILE = "C:\\Data\\case_temperature.sensor"
 
 KEY_PATH = r"Software\HWiNFO64\Sensors\Custom\Custom Sensors\Temp0"
+
+IDLE_TIME = 120       # 120 seconds without input
+CPU_THRESHOLD = 10.0  # below 10%
+
+
+message = "System idle detected"
+
+
+def is_user_idle():
+    # reuse GetLastInputInfo
+    import ctypes
+    class LASTINPUTINFO(ctypes.Structure):
+        _fields_ = [("cbSize", ctypes.c_uint),
+                    ("dwTime", ctypes.c_uint)]
+    last_input = LASTINPUTINFO()
+    last_input.cbSize = ctypes.sizeof(last_input)
+    ctypes.windll.user32.GetLastInputInfo(ctypes.byref(last_input))
+    millis = ctypes.windll.kernel32.GetTickCount() - last_input.dwTime
+    return millis / 1000 > IDLE_TIME
+
+def is_cpu_idle():
+    return psutil.cpu_percent(interval=1) < CPU_THRESHOLD
 
 def find_sensor_port():
     ports = serial.tools.list_ports.comports()
@@ -42,7 +72,14 @@ def update_registry(value):
 
 def read_sensor(port):
     with serial.Serial(port, BAUD, timeout=2) as ser:
+
         while True:
+
+            if is_user_idle() and is_cpu_idle():
+                print("System is idle (Task Scheduler conditions)")
+                # if system is idle, break to main loop, disconnect sensor
+                break
+
             try:
                 line = ser.readline().decode(errors="ignore").strip()
                 if MAGIC in line:
@@ -64,6 +101,16 @@ def read_sensor(port):
 
 def main():
     while True:
+
+        if is_user_idle() and is_cpu_idle():
+            print("System is idle (Task Scheduler conditions)")
+            # Generate ISO timestamp
+            timestamp = datetime.now().isoformat(timespec='seconds')
+            with open(IDL_FNAME, "a", encoding="utf-8") as f:
+                f.write(f"{timestamp} - {message}\n")
+            time.sleep(10)
+            continue
+
         port = find_sensor_port()
 
         if port:
